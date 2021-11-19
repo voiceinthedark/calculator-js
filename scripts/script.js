2805c26a9008f97fc240f2f65853b3850ac951a5
voiceinthedark <voiceinthedark@users.noreply.github.com>
2021-11-19 19:03:01 +0200

    - Fix bug where user could input many decimal dot - Fix output display of only 2 decimal digits after the comma - Add lodash library dependency

--- scripts/script.js
+++ scripts/script.js
@@ -4,10 +4,10 @@
 class Calculator {
   constructor() {
     this._mather = {
-      "+": (a, b) => +a + +b,
-      "-": (a, b) => +a - +b,
+      '+': (a, b) => +a + +b,
+      '-': (a, b) => +a - +b,
       'x': (a, b) => +a * +b,
-      "/": (a, b) => +a / +b,
+      '/': (a, b) => +a / +b,
     };
   }
 
@@ -17,7 +17,7 @@ class Calculator {
 
   calculate(str) {
     // console.log(str);
-    return this._switcher(...str.split(" "));
+    return this._switcher(...str.split(' '));
   }
 
   addMethod(name, fn) {
@@ -31,14 +31,13 @@ const calculator = new Calculator();
  * Elements capture
  */
 const displayHeader = document.querySelector('.display-header');
-const displayValue = document.querySelector(".display-value");
-const numbersButtons = document.querySelectorAll(".number");
-const operatorsButtons = document.querySelectorAll(".operator");
+const displayValue = document.querySelector('.display-value');
+const numbersButtons = document.querySelectorAll('.number');
+const operatorsButtons = document.querySelectorAll('.operator');
 const clearButton = document.querySelector('#ce');
 const plusMinusButton = document.querySelector('.plusmin');
 const equalButton = document.querySelector('#equal');
 const deleteButton = document.querySelector('#del');
-
 
 let arrayOfNumbers = [];
 let number = 0;
@@ -52,45 +51,49 @@ equationHistory = {
   result: null,
 };
 
-
 /**
  * Numbers button methods
  */
 numbersButtons.forEach((button) => {
-  button.addEventListener("click", (e) => {
-    arrayOfNumbers.push(e.target.textContent);
-    displayValue.textContent = arrayOfNumbers.join("");    
+  button.addEventListener('click', (e) => {
+    if (e.target.textContent === '.' && arrayOfNumbers.includes('.')) {
+    } else {
+      arrayOfNumbers.push(e.target.textContent);
+      displayValue.textContent = arrayOfNumbers.join('');
+      // displayHeader.textContent = arrayOfNumbers.join('');
+    }
   });
 });
 
 operatorsButtons.forEach((button) => {
-  button.addEventListener("click", (e) => {
+  button.addEventListener('click', (e) => {
     // debugger;
-    if(equationHistory.left == null){
-      number = Number(arrayOfNumbers.join(''));        
+    if (equationHistory.left == null) {
+      number = Number(arrayOfNumbers.join(''));
       operator = e.target.textContent;
-      if(equationHistory.result){
+      if (equationHistory.result) {
         equationHistory.left = equationHistory.result;
         displayValue.textContent = equationHistory.left;
-        equationHistory.right =  number;
+        equationHistory.right = number;
         equationHistory.result = _calculate(equationHistory);
       } else {
         equationHistory.left = number;
       }
-      equationHistory.operator = operator;      
-      // console.log(equationHistory);
+      equationHistory.operator = operator;
+      displayHeader.textContent += operator;
       arrayOfNumbers = [];
-      displayHeader.textContent = operator;  
     } else {
-      number = Number(arrayOfNumbers.join("")); 
+      number = Number(arrayOfNumbers.join(''));
       equationHistory.right = number;
+      displayHeader.textContent += equationHistory.right;
       result = _calculate(equationHistory);
       operator = e.target.textContent;
       equationHistory.operator = operator;
+      displayHeader.textContent += operator;
       // console.log(equationHistory);
       equationHistory.result = result;
       equationHistory.left = equationHistory.result;
-      equationHistory.right = null;      
+      equationHistory.right = null;
     }
   });
 });
@@ -105,7 +108,7 @@ clearButton.addEventListener('click', (e) => {
     right: null,
     result: null,
   };
-  displayHeader.textContent = "";
+  displayHeader.textContent = '';
   displayValue.textContent = 0;
   arrayOfNumbers = [];
   number = 0;
@@ -115,10 +118,10 @@ clearButton.addEventListener('click', (e) => {
 });
 
 /**
-* Plus/minus button method
-*/
+ * Plus/minus button method
+ */
 plusMinusButton.addEventListener('click', (e) => {
-  if(displayValue.textContent.startsWith('-')){
+  if (displayValue.textContent.startsWith('-')) {
     displayValue.textContent = displayValue.textContent.slice(1);
     arrayOfNumbers.shift();
   } else {
@@ -133,7 +136,7 @@ plusMinusButton.addEventListener('click', (e) => {
 equalButton.addEventListener('click', (e) => {
   number = Number(arrayOfNumbers.join(''));
   equationHistory.right = number;
-  result = _calculate(equationHistory);  
+  result = _calculate(equationHistory);
   // console.log(equationHistory);
   displayValue.textContent = result;
   equationHistory.left = null;
@@ -143,12 +146,14 @@ equalButton.addEventListener('click', (e) => {
 
 deleteButton.addEventListener('click', (e) => {
   console.log('in del');
-  if(displayValue.textContent.length > 1){
+  if (displayValue.textContent.length > 1) {
     console.log(displayValue.textContent);
-    displayValue.textContent = displayValue.textContent.slice(0,-1);
+    displayValue.textContent = displayValue.textContent.slice(0, -1);
     arrayOfNumbers.pop();
-    equationHistory.result = parseFloat((''+equationHistory.result).slice(0, -1));
-  } else if(displayValue.textContent.length === 1){
+    equationHistory.result = parseFloat(
+      ('' + equationHistory.result).slice(0, -1)
+    );
+  } else if (displayValue.textContent.length === 1) {
     displayValue.textContent = 0;
     // displayValue.textContent = arrayOfNumbers.join('');
     arrayOfNumbers[0] = 0;
@@ -158,21 +163,22 @@ deleteButton.addEventListener('click', (e) => {
 
 /**
  * Function to calculate the result of the equation; expects an object
- * @param {equationHistory} eq 
+ * @param {equationHistory} eq
  */
 
 function _calculate(eq) {
-
   if (arrayOfNumbers.length > 0) {
-    equationResult = calculator.calculate(`${eq.left} ${eq.operator} ${eq.right}`);
-    if(Number.isInteger(equationResult)){
+    equationResult = calculator.calculate(
+      `${eq.left} ${eq.operator} ${eq.right}`
+    );
+    if (Number.isInteger(equationResult)) {
       displayValue.textContent = equationResult;
     } else {
-      displayValue.textContent = equationResult.toFixed(2);    
+      displayValue.textContent = equationResult.toFixed(2);
     }
-    arrayOfNumbers = [];    
+    arrayOfNumbers = [];
     eq.result = equationResult;
   }
-  return equationResult;
+  // displayHeader.textContent = '';
+  return _.round(equationResult, 2);
 }
-
